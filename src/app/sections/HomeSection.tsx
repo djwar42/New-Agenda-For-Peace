540086b6336c74d71450baa84e2c359eff20c199
Daniel <happyrobotstudio@icloud.com>
2025-03-11 09:59:15 +1100

    Update HomeSection.tsx

--- src/app/sections/HomeSection.tsx
+++ src/app/sections/HomeSection.tsx
@@ -14,22 +14,22 @@ export default function HomeSection() {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.5 }}
-      className='w-screen bg-[radial-gradient(circle_at_center_top,_#009EDB,_#4DB6E6,_#1F2A44)] text-white py-20'
+      className='bg-[radial-gradient(circle_at_center_top,_#009EDB,_#4DB6E6,_#1F2A44)] text-white flex items-center justify-center py-20'
     >
       <div className='max-w-7xl mx-auto px-4 md:px-6 lg:px-12 text-center'>
         <motion.div
           initial={{ scale: 0.8 }}
           animate={{ scale: 1 }}
           transition={{ duration: 0.3 }}
-          className='mb-8 inline-block'
+          className='mb-12 inline-block'
         >
-          <Earth size={150} color='#FFFFFF' />
+          <Earth size={180} color='#FFFFFF' />
         </motion.div>
         <motion.h1
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 0.5, delay: 0.1 }}
-          className='text-5xl md:text-7xl font-extrabold mb-6'
+          className='text-6xl md:text-8xl font-extrabold mb-8'
         >
           A New Agenda for Peace
         </motion.h1>
@@ -37,13 +37,13 @@ export default function HomeSection() {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 0.5, delay: 0.2 }}
-          className='text-xl md:text-2xl max-w-4xl mx-auto mb-8'
+          className='text-2xl md:text-3xl max-w-5xl mx-auto mb-12'
         >
           Unveiled on 20 July 2023 by UN Secretary-General António Guterres,
           this transformative policy brief presents a comprehensive strategy to
           bolster multilateral efforts for peace. Stemming from{' '}
           <i>Our Common Agenda</i> and the 75th anniversary declaration, it
-          confronts a world marked by geopolitical shifts, escalating tensions
+          confronts a world marked by geopolitical shifts, escalating tensions,
           and climate crises. It calls for renewed global cooperation to address
           these intertwined challenges, emphasizing the urgency of collective
           action to fulfill the UN Charter’s promise of peace, sustainable
@@ -52,7 +52,7 @@ export default function HomeSection() {
         <motion.div
           whileHover={{ scale: 1.1 }}
           transition={{ duration: 0.3 }}
-          className='cursor-pointer mb-8 inline-block'
+          className='cursor-pointer mb-12 inline-block'
           onClick={scrollToNext}
         >
           <ArrowDown size={60} color='#FFFFFF' />
